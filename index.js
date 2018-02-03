974db331f3a521b3338aa685b1aeeae49821ff3c
Ahmad Samiei <ahmad.samiei@gmail.com>
2018-02-03 15:59:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+
+const statusCodes = {};
+
+function setStatusCode(tabId) {
+    let statusCode = statusCodes[tabId];
+    if (!statusCode) {
+        browser.browserAction.setBadgeText({ text: '' });
+        return;
+    }
+
+    browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+        if (tabs[0].id !== tabId) {
+            return;
+        }
+
+        browser.browserAction.setBadgeText({ text: statusCode.toString() });
+        browser.browserAction.setBadgeBackgroundColor({ color: getStatusCodeColor(statusCode) });
+    });
+}
+
+function getStatusCodeColor(x) {
+    switch (true) {
+        case (x < 200):
+            return 'blue';
+            break;
+        case (x >= 200 && x < 300):
+            return 'darkgreen';
+            break;
+        case (x >= 300 && x < 400):
+            return 'purple';
+            break;
+        case (x >= 400 && x < 500):
+            return 'chocolate';
+            break;
+        default:
+            return 'red'
+            break;
+    }
+}
+
+browser.webRequest.onCompleted.addListener(
+    e => {
+        statusCodes[e.tabId] = e.statusCode;
+        setStatusCode(e.tabId)
+    },
+    { urls: ["<all_urls>"] }
+);
+
+browser.webNavigation.onCommitted.addListener(e => {
+    if (e.frameId === 0) {
+        setStatusCode(e.tabId);
+    }
+});
+
+browser.tabs.onActivated.addListener(e => {
+    setStatusCode(e.tabId);
+});
+
+browser.tabs.onRemoved.addListener(tabId => {
+    statusCodes[tabId] = null;
+});
