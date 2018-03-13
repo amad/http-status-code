6aea8298b792e389da4a48971ba1650b72acee75
Ahmad Samiei <ahmad.samiei@gmail.com>
2018-03-13 19:04:53 +0100

    Reformat index.js

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
-
 const statusCodes = {};
 
 function setStatusCode(tabId) {
-    let statusCode = statusCodes[tabId];
-    if (!statusCode) {
-        browser.browserAction.setBadgeText({ text: '' });
-        return;
+  let statusCode = statusCodes[tabId];
+  if (!statusCode) {
+    browser.browserAction.setBadgeText({ text: '' });
+    return;
+  }
+
+  browser.tabs.query({ currentWindow: true, active: true }).then(tabs => {
+    if (tabs[0].id !== tabId) {
+      return;
     }
 
-    browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
-        if (tabs[0].id !== tabId) {
-            return;
-        }
-
-        browser.browserAction.setBadgeText({ text: statusCode.toString() });
-        browser.browserAction.setBadgeBackgroundColor({ color: getStatusCodeColor(statusCode) });
+    browser.browserAction.setBadgeText({ text: statusCode.toString() });
+    browser.browserAction.setBadgeBackgroundColor({
+      color: getStatusCodeColor(statusCode)
     });
+  });
 }
 
 function getStatusCodeColor(x) {
-    switch (true) {
-        case (x < 200):
-            return 'blue';
-            break;
-        case (x >= 200 && x < 300):
-            return 'darkgreen';
-            break;
-        case (x >= 300 && x < 400):
-            return 'purple';
-            break;
-        case (x >= 400 && x < 500):
-            return 'chocolate';
-            break;
-        default:
-            return 'red'
-            break;
-    }
+  switch (true) {
+    case x < 200:
+      return 'blue';
+      break;
+    case x >= 200 && x < 300:
+      return 'darkgreen';
+      break;
+    case x >= 300 && x < 400:
+      return 'purple';
+      break;
+    case x >= 400 && x < 500:
+      return 'chocolate';
+      break;
+    default:
+      return 'red';
+      break;
+  }
 }
 
 browser.webRequest.onCompleted.addListener(
-    e => {
-        if (e.tabId === -1 || e.type !== 'main_frame') {
-            return;
-        }
+  e => {
+    if (e.tabId === -1 || e.type !== 'main_frame') {
+      return;
+    }
 
-        statusCodes[e.tabId] = e.statusCode;
-        browser.browserAction.setTitle({ tabId: e.tabId, title: e.statusLine });
-        setStatusCode(e.tabId)
-    },
-    { urls: ["<all_urls>"] }
+    statusCodes[e.tabId] = e.statusCode;
+    browser.browserAction.setTitle({ tabId: e.tabId, title: e.statusLine });
+    setStatusCode(e.tabId);
+  },
+  { urls: ['<all_urls>'] }
 );
 
 browser.webNavigation.onCommitted.addListener(e => {
-    if (e.frameId === 0) {
-        setStatusCode(e.tabId);
-    }
+  if (e.frameId === 0) {
+    setStatusCode(e.tabId);
+  }
 });
 
 browser.tabs.onActivated.addListener(e => {
-    setStatusCode(e.tabId);
+  setStatusCode(e.tabId);
 });
 
 browser.tabs.onRemoved.addListener(tabId => {
-    statusCodes[tabId] = null;
+  statusCodes[tabId] = null;
 });
